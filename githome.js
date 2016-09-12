5ef8b24a44f4a404a9c172fbf326e0ff68f7bb4b
Alex <alexandercheney@Alexanders-MacBook-Pro-2.local>
2016-09-12 13:09:17 -0700

    Minor Tweaks to excel formatting/clean-up

--- githome.js
+++ githome.js
@@ -2,12 +2,15 @@ var request = require('request');
 var async = require('async');
 var sleep = require('sleep');
 var readline = require('readline');
+var excelbuilder = require('msexcel-builder');
+
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 var summonerID;
+
 //function that returns all-time winrate
 function doTotalWinrate(Sessions){
 	var wins=0;
@@ -23,11 +26,13 @@ function doTotalWinrate(Sessions){
 	if (total===0){
 		return "No Games Found";
 	}
+	console.log(total);
 	wins= (wins*100)/total;
 	wins= wins - (wins%1);
-	return wins+"%.";
+	var answer = [wins+"%", total];
+	return answer;
 }
-//NEED TO FIX THIS
+
 //function that returns the average winrate for a play session with <number> games
 function doSessionLengths(Sessions, number){
 	//how many wins in sessions of <number> length
@@ -51,9 +56,123 @@ function doSessionLengths(Sessions, number){
 	//total * number is the total number of games played in sessions of <number>length
 	wins=(wins*100)/(total*number);
 	wins= wins - (wins%1);
-	return wins+"% with a sample size of "+ total+ " sessions.";
+	var answer=[wins, total];
+	return answer;//" with a sample size of "+ total+ " sessions.";
 }
 
+//distill doWinAfterOne,Two,Three into one function 
+
+//function that returns winrate following the first three losses in any play session
+function doWinAfterThree(Sessions){
+	//tracks number of wins in play sessions after losing three games 
+	var winAfterThree=0;
+	//tracks total number of games in play sessions after the third loss
+	var totalAfterThree=0;
+	//tracks how many losses the player has experienced in the current session
+	var loss;
+	//sample size
+	var size=0;
+	for (i in Sessions){
+		//reset loss number to 0 for the next session
+		loss=0;
+		for (var j = Sessions[i].length-1; j >0; j--){
+			//if the game is a loss and we haven't seen a loss yet in the current session
+			if (Sessions[i][j]===false && loss===0){
+				loss=1;
+			}
+			//else if we've already seen a loss in the current session
+			else if (Sessions[i][j]===false && loss===1){
+				loss=2;
+			}
+			else if (Sessions[i][j]===false && loss===2){
+				loss=3;
+				size++;
+			}
+			//else if we've already seen two losses in the current session
+			else if(loss===3){
+				//increment winAfterTwo if the player wins
+				if (Sessions[i][j]===true){
+					winAfterThree++;
+				}
+				//always increment this after the second loss
+				totalAfterThree++;
+			}
+		}
+	}
+	var answer = [winAfterThree,totalAfterThree];
+	return answer;
+}
+
+//function that returns winrate following the two losses in any play session
+function doWinAfterTwo(Sessions){
+	//tracks number of wins in play sessions after first two losses
+	var winAfterTwo=0;
+	//tracks total number of games in play sessions after two losses
+	var totalAfterTwo=0;
+	//tracks how many losses already experienced in the current play session
+	var loss;
+	//sample size
+	var size=0;
+	for (i in Sessions){
+		//reset loss number to 0 for the next session
+		loss=0;
+		for (var j = Sessions[i].length-1; j >0; j--){
+			//if the game is a loss and we haven't seen a loss yet in the current session
+			if (Sessions[i][j]===false && loss===0){
+				loss=1;
+			}
+			//else if we've already seen a loss in the current session
+			else if (Sessions[i][j]===false && loss===1){
+				loss=2;
+				size++;
+			}
+			//else if we've already seen two losses in the current session
+			else if(loss===2){
+				//increment winAfterTwo if the player wins
+				if (Sessions[i][j]===true){
+					winAfterTwo++;
+				}
+				//always increment this after the second loss
+				totalAfterTwo++;
+			}
+		}
+	}
+	var answer = [winAfterTwo,totalAfterTwo];
+	return answer;
+}
+
+//function that returns winrate following the first loss in any play session
+function doWinAfterOne(Sessions){
+	//tracks number of wins in play sessions after first loss
+	var winAfterOne=0;
+	//tracks total number of games in play sessions after a loss
+	var totalAfterOne=0;
+	//boolean to track if we've already seen a loss in the current play session
+	var loss;
+	//sample size
+	var size=0;
+	for (i in Sessions){
+		loss=0;
+		for (var j = Sessions[i].length-1; j >0; j--){
+			//if the game is a loss and we haven't seen a loss yet in the current session
+			if (Sessions[i][j]===false&&loss===0){
+				loss=1;
+				size++;
+			}
+			//else if we've already seen a loss in the current session
+			else if(loss===1){
+				//increment winAfterOne if the player wins
+				if (Sessions[i][j]===true){
+					winAfterOne++;
+				}
+				//always increment this after the first loss
+				totalAfterOne++;
+			}
+		}
+	}
+	var answer = [winAfterOne,totalAfterOne];
+	return answer;
+}
 //function that returns winrate of the first game if the final game of the previous session was a loss
 function doWinAfterBreak(Sessions){
 	//tracks the number of wins after a break following a loss
@@ -69,11 +188,16 @@ function doWinAfterBreak(Sessions){
 			}
 		}
 	}
+	if (totalAfterBreak===0){
+		return "Not Enough Data";
+	}
+	var answer = [0,winAfterBreak];
 	//calculate winning percentage for final game of play session
 	winAfterBreak=(winAfterBreak*100)/totalAfterBreak;
 	//shave off decimals
 	winAfterBreak=winAfterBreak-(winAfterBreak%1);
-	return winAfterBreak+"%";
+	answer[0]=winAfterBreak;
+	return answer;
 }
 //Looks at winrate immediately following a loss
 function doImmediateTwo(Sessions){
@@ -132,16 +256,33 @@ function doImmediateOne(Sessions){
 	if (totalAfterLoss===0){
 		return "Not Enough Data";
 	}
+	var answer =[0,winAfterLoss];
 	winAfterLoss= ((winAfterLoss*100)/totalAfterLoss);
 	winAfterLoss= winAfterLoss - (winAfterLoss%1); 
-	return winAfterLoss + '%';
+	answer[0]=winAfterLoss;
+	return answer;
+}
+
+//NOT USED YET
+//Looks at winrate of next three games following a loss across all play Sessions
+function doClusterOne (Sessions){
+	var total;
+	for (i in Sessions){
+		//need at least 4 games to analyze a loss and the three cluster games after that.
+		if (Sessions[i].length>3){
+			for (var j = Sessions[i].length-1; j >0; j--){
+				if (Sessions[i][j]===false){
+
+					total++;
+				}
+			}
+		}
+	}
 }
 
 
 
-
-
-//STILL NEEDS WORK
+//NOT USED YET
 //Looks to see if losing two games in the middle of a play session negatively affects winrate afterwards
 function doPivotTwo(Sessions){
 	for (i in Sessions){
@@ -160,7 +301,7 @@ function doPivotTwo(Sessions){
 
 
 }
-//STILL NEEDS WORK
+//NOT USED YET
 //Looks at a loss in the middle of the play session to compare winrate before and after the loss
 function doPivotOne(Sessions){
 	//total number of wins before the loss pivot
@@ -225,46 +366,6 @@ function doTwoThirdsLoss(Sessions){
 	}
 	return twoThirds+ "% (sample size "+num+" sessions)";
 }
-//NOT USED currently
-//function that returns the average winrate in play sessions after losing 1/3 first games
-function doOneThirdsLoss(Sessions){
-	var oneThirds =0;
-	var oneThirdsTotal=0;
-	//console.log(Sessions);
-	var num=0;
-	for (i in Sessions){
-		//if at least four games were played in a row
-		if (Sessions[i].length >3){
-			//if they lost two of their first three games in the sessions
-			var count=0;
-			for (var j =Sessions[i].length-1; j>=Sessions[i].length-3; j--){
-				
-				if (Sessions[i][j]===true){
-					count++;
-				}
-			}
-			//if one win happens during the first three games
-			if (count===1){
-				//loop through the remaining games
-				for (var j = Sessions[i].length-4; j>= 0; j--){
-					if (Sessions[i][j]===true) {
-						oneThirds++;
-					}
-					oneThirdsTotal++;
-				}
-			}
-			//tally the reamining wins and losses from the session
-		}
-	}
-	//calculate winrate for the remainder of the play sessions after losing the first game
-	twoThirds=((twoThirds*100)/twoThirdsTotal);
-	//shave off decimals
-	twoThirds= twoThirds - (twoThirds%1);
-	if (twoThirdsTotal===0){
-		return "Not Enough Data";
-	}
-	return twoThirds+ "% (sample size "+num+" sessions) ";
-}
 
 //function that returns the average winrate in play sessions after losing the first three games
 function doThreeLoss(Sessions){
@@ -295,7 +396,8 @@ function doThreeLoss(Sessions){
 	if (threeTotal===0){
 		return "Not Enough Data";
 	}
-	return threeLoss+ "% (sample size "+num+" sessions) ";
+	var answer = [threeLoss,num];
+	return answer;
 }
 
 //function that returns the average winrate in play sessions after losing the first two games
@@ -327,7 +429,8 @@ function doTwoLoss(Sessions){
 	if (twoTotal===0){
 		return "Not Enough Data";
 	}
-	return twoLoss+ "% (sample size "+num+" sessions) ";
+	var answer = [twoLoss,num];
+	return answer;
 }
 
 //function that returns the average winrate in play sessions after losing the first game
@@ -361,24 +464,118 @@ function doOneLoss(Sessions){
 	if (oneTotal===0){
 		return "Not Enough Data";
 	}
-	return oneLoss + "% with a sample size of "+num+" sessions";
+	var answer = [oneLoss,num];
+	return answer;
 }
-
-//function that outputs the results of all analytic functions
+//function that outputs the results of all analytic functions to the Terminal and to an Excel doc
 function doAnalysis(Sessions){
-console.log(Sessions);
-	console.log("Total Winrate: "+ doTotalWinrate(Sessions));
-	console.log("If you lose the first game of a ranked session, your average winrate is "+doOneLoss(Sessions) +" for the remaining games.");
+	var answer;
+	console.log(Sessions);
+	var workbook = excelbuilder.createWorkbook('./', summonerID+'.xlsx');
+	var sheet1 = workbook.createSheet('sheet1', 20, 20);
+	sheet1.set(1,1, "SummonerID");
+	sheet1.set(2,1, "Total win-rate");
+	sheet1.set(3,1, "Winrate after losing first game");
+	sheet1.set(4,1, "Winrate after losing first two games");
+	sheet1.set(5,1, "Winrate after losing first three games");
+	sheet1.set(6,1, "Winrate following a break after a loss");
+	sheet1.set(7,1, "Winrate immediately after a loss");
+	sheet1.set(8,1, "Winrate over one-game sessions");
+	sheet1.set(9,1, "Winrate over two-game sessions");
+	sheet1.set(10,1, "Winrate over three-game sessions");
+	sheet1.set(11,1, "Winrate over four-game sessions");
+	sheet1.set(12,1, "Winrate over five-game sessions");
+	sheet1.set(13,1, "Winrate over six-game sessions");
+	sheet1.set(14,1, "Winrate over seven-game sessions");
+	sheet1.set(15,1, "Winrate over eight-game sessions");
+	sheet1.set(16,1, "Winrate over nine-game sessions");
+	sheet1.set(17,1, "Winrate over ten-game sessions");
+	sheet1.set(1,3, "Sample Size");
+
+	sheet1.set(1,2,summonerID+'');
+	answer = doTotalWinrate(Sessions);
+	sheet1.set(2,2, answer[0];
+	sheet1.set(2,3, answer[1]);
+
+	answer = doOneLoss(Sessions);
+	sheet1.set(3,2,answer[0]+'%');
+	sheet1.set(3,3,answer[1]);
+
+	answer = doTwoLoss(Sessions);
+	sheet1.set(4,2, answer[0]+'%');
+	sheet1.set(4,3, answer[1]);
+
+	answer = doThreeLoss(Sessions);
+	sheet1.set(5,2, answer[0]+'%');
+	sheet1.set(5,3, answer[1]);
+
+	answer = doWinAfterBreak(Sessions);
+	sheet1.set(6,2, answer[0]+'%');
+	sheet1.set(6,3, answer[1]);
+
+	answer = doImmediateOne(Sessions);
+	sheet1.set(7,2, answer[0]+'%');
+	sheet1.set(7,3, answer[1]);
+	
+	answer=doSessionLengths(Sessions,1);
+	sheet1.set(8,2, answer[0]+'%');
+	sheet1.set(8,3, answer[1]);
+
+	answer=doSessionLengths(Sessions,2);
+	sheet1.set(9,2,answer[0]+'%');
+	sheet1.set(9,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,3);
+	sheet1.set(10,2,answer[0]+'%');
+	sheet1.set(10,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,4);
+	sheet1.set(11,2,answer[0]+'%');
+	sheet1.set(11,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,5);
+	sheet1.set(12,2,answer[0]+'%');
+	sheet1.set(12,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,6);
+	sheet1.set(13,2,answer[0]+'%');
+	sheet1.set(13,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,7);
+	sheet1.set(14,2,answer[0]+'%');
+	sheet1.set(14,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,8);
+	sheet1.set(15,2,answer[0]+'%');
+	sheet1.set(15,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,9);
+	sheet1.set(16,2,answer[0]+'%');
+	sheet1.set(16,3,answer[1]);
+
+	answer=doSessionLengths(Sessions,10);
+	sheet1.set(17,2,answer[0]+'%');
+	sheet1.set(17,3,answer[1]);
+	
+
+
+	
+	//console log
+	answer= doTotalWinrate(Sessions);
+	console.log("Total Winrate: "+ answer);
+
+	answer=doOneLoss(Sessions);
+	console.log("If you lose the first game of a ranked session, your average winrate is "+ doOneLoss(Sessions) + "% for the remaining games.");
 
 	console.log("If you lose the first two games of a ranked session, your average winrate is "+doTwoLoss(Sessions) +" for the remaining games.");
 
 	console.log("If you lose the first three games of a ranked session, your average winrate is "+doThreeLoss(Sessions) +" for the remaining games.");
 
-	console.log("If you lose two of your first three games of a ranked session, your average winrate is "+doTwoThirdsLoss(Sessions)+" for the remaining games");
+	//console.log("If you lose two of your first three games of a ranked session, your average winrate is "+doTwoThirdsLoss(Sessions)+" for the remaining games");
 
 	console.log('Following a break after ending a ranked session on a loss, your winrate is '+doWinAfterBreak(Sessions));
 	console.log('Your winrate immediately following a loss is: '+ doImmediateOne(Sessions));
-	console.log('Your winrate immediately following two or more losses is: '+ doImmediateTwo(Sessions));
+	//console.log('Your winrate immediately following two or more losses is: '+ doImmediateTwo(Sessions));
 	//gets winrates for play sessions of 1-5 games longs
 	console.log("Your average winrate when playing exactly one game is "+ doSessionLengths(Sessions, 1));
 	console.log("Your average winrate when playing exactly two games is "+ doSessionLengths(Sessions, 2));
@@ -390,8 +587,19 @@ console.log(Sessions);
 	console.log("Your average winrate when playing exactly eight games is "+ doSessionLengths(Sessions, 8));
 	console.log("Your average winrate when playing exactly nine games is "+ doSessionLengths(Sessions, 9));
 	console.log("Your average winrate when playing exactly ten games is "+ doSessionLengths(Sessions, 10));
-
-}
+	
+	console.log(doWinAfterOne(Sessions));
+	console.log(doWinAfterTwo(Sessions));
+	console.log(doWinAfterThree(Sessions));
+	
+	//saves Excel spreadsheet
+	workbook.save(function(ok){
+    	if (!ok) 
+    		workbook.cancel();
+   		else
+    		console.log('congratulations, your workbook created');
+	})
+};
 
 //function that sorts the matches into play sessions via timestamp
 var groupTimeStamps = function(matchlist, time){
@@ -401,23 +609,26 @@ var groupTimeStamps = function(matchlist, time){
 	playSession=[];
 	//for each match
 	var length = matchlist.length-1;
+	//length=10;
 	//variable that stores the split interval between two games
 	var split = time*1000;
-	//if (length>100){
-	//	length=100;
+	//if (length>4000){
+	//	length=40;
 	//}
 	for (var i =0; i < length; i++){
-		console.log(matchlist[i].timestamp);
+	//	if (matchlist[i].timestamp >1468872000000){
+		console.log(matchlist[i].matchId);
 		//add it to a playSession list
 		playSession.push(matchlist[i].matchId);
 		//if the previous match's timestamp is not within one hour of the current one
 		if (matchlist[i].timestamp -split > matchlist[i+1].timestamp){
-		//	console.log();
+			
 			//add the playSession to the list of play sessions
 			sortedMatches.push(playSession);
 			//reset the playSession list
 			playSession=[];
 		}
+	//}
 	}
 	return sortedMatches;
 }
@@ -429,7 +640,7 @@ var convertWL = function(playSesh){
 
 	 
 		
-		console.log("Checking play session "+i);
+		console.log("Checking play session "+(i+1));
 		console.log("There are "+ playSesh[i].length +" games played");
 
 		//Implement asynchronization so secondary API Calls return in-order
@@ -437,11 +648,12 @@ var convertWL = function(playSesh){
 				
 				//playSesh[i][j]= getWL(game, callback2);
 				getWL(game, function(result){
+					//convert into array of results+timestamp. Want to eventually be able to re-group based on different time separations
 					playSesh[i][j] = result;
 					callback2();
 				});
 		}, callback);
-		//call Analysis function
+		//Call Analysis function. Actually want to groupTimeStamps again based on average time for the player
 	}, function(){doAnalysis(playSesh);});
 	
 }
@@ -449,8 +661,9 @@ var convertWL = function(playSesh){
 //function that helps convertWL by making an API Call for a single matchID
 var getWL= function(game, callback){
 	//set game to the request
-	sleep.usleep(1200001);
-	request('https://na.api.pvp.net/api/lol/na/v2.2/match/'+game+'?api_key=<API_KEY>', function (error, response, body){
+	
+	request('https://na.api.pvp.net/api/lol/na/v2.2/match/'+game+'?api_key=', function (error, response, body){
+		sleep.usleep(2000001);
 		var result;
 		if(!error && response.statusCode ==200){
 			var one= JSON.parse(body);
@@ -463,33 +676,47 @@ var getWL= function(game, callback){
 				}
 
 			}
-				console.log(one.matchCreation+" "+ one.matchDuration);
+				
+		}
+		else{
+			console.log(response.statusCode, "error", game);
 		}
 		callback(result);
 	});
 	
 }
+
+//get SummonerID from terminal prompt
 rl.question("summonerID: ", (answer) =>{
 summonerID=answer+'';
 summonerID=parseFloat(summonerID);
 
 //original API Call for Summoner ID
-request('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/'+summonerID+'?rankedQueues=RANKED_SOLO_5x5,TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=PRESEASON3,SEASON3,PRESEASON2014,SEASON2014,PRESEASON2015,SEASON2015,PRESEASON2016,SEASON2016&api_key=<API_KEY>', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
+request('https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/'+summonerID+'?rankedQueues=RANKED_SOLO_5x5,TEAM_BUILDER_DRAFT_RANKED_5x5&seasons=PRESEASON3,SEASON3,PRESEASON2014,SEASON2014,PRESEASON2015,SEASON2015,PRESEASON2016,SEASON2016&api_key=', function (error, response, body) {
+  if (!error && response.statusCode == 200 && body !=undefined) {
     var jsonBody = JSON.parse(body);
-    sleep.usleep(1200001);
-    //for (i in jsonBody.matches)
-    //{
-    	
-    	//console.log(jsonBody.matches[i].matchId);
-    	//console.log(jsonBody.matches[i].timestamp);	
-    //}
-    var sessions = groupTimeStamps(jsonBody.matches, 3600);
+    sleep.usleep(2000001);
+    
+    if (jsonBody.matches!=undefined){
+    	if (jsonBody.matches.length <10){
+    		console.log("under 100 games played: "+jsonBody.matches.length);
+    	}
+    
+    	else{
+   			var sessions = groupTimeStamps(jsonBody.matches, 5400);
+    		console.log(sessions.length+ " play sessions");
+    		convertWL(sessions);
 
-    console.log(sessions.length+ " play sessions");
-    convertWL(sessions);
-
+		}
+	}
+	else{
+		console.log("error, ranked history is undefined");
+	}
   }
 });
+	
+
 rl.close();
 });
+//}
+
